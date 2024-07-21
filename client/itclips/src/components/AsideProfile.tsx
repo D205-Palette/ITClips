fca3c43cec18f2c12599cc04ab126f2ff61ae609
김세진 <rlatpwls30@naver.com>
2024-07-21 22:25:36 +0900

    comment: Aside 프로필 컴포넌트 자세한 주석 추가

--- client/itclips/src/components/AsideProfile.tsx
+++ client/itclips/src/components/AsideProfile.tsx
@@ -1,5 +1,6 @@
 import profileImage from "../assets/profile_image.png"
 
+// 일단 API 어떻게 들어올지 몰라서 임시로 받는 값 만들어 둠
 interface ProfileCardProps {
   username: string;
   email: string;
@@ -21,15 +22,18 @@ const ProfileCard = ({
 }: ProfileCardProps) => {
   return (
     <div className="bg-aside-layout rounded-3xl w-80 p-8 flex flex-col items-center">
+      {/* 더보기 버튼 */}
       <div className="self-end">
         <button type="button" className="text-center">...</button>
       </div>
+      {/* 프로필 이미지 */}
       <div className="mb-4">
         <img src={profileImage} className="w-16 h-16 bg-gray-200 rounded-full mb-4"></img>
       </div>
       <h2 className="text-xl font-bold mb-1">{username}</h2>
       <p className="text-gray-500 mb-2">{email}</p>
       <p className="text-center text-sm mb-6">{description}</p>
+      {/* 팔로워, 팔로잉 리스트, 북마크 수 출력부분 */}
       <div className="w-full grid grid-cols-1 gap-y-4">
         <div className="grid grid-cols-6">
           <div className="col-start-2 col-span-2 text-start text-gray-500">팔로워</div>

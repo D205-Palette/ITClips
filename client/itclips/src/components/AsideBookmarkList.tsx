7d9795e69cb85472bdbe348da9494026629691e9
김세진 <rlatpwls30@naver.com>
2024-07-22 12:50:33 +0900

    refactor: [Aside] 북마크리스트 더미 데이터 생성

--- client/itclips/src/components/AsideBookmarkList.tsx
+++ client/itclips/src/components/AsideBookmarkList.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import profileImage from "../assets/profile_image.png";
-import heart from "../assets/heart.png";
-import star from "../assets/star.png";
+import { VscKebabVertical } from "react-icons/vsc";
+import { FaRegHeart, FaRegStar } from "react-icons/fa";
 
 interface Comment {
   id: number;
@@ -13,9 +12,9 @@ interface Tag {
   text: string;
 }
 
-interface ProfileCardProps {
+interface BookmarkListProps {
   listName: string;
-  username: string;
+  email: string;
   description: string;
   likeCount: number;
   starCount: number;
@@ -23,51 +22,69 @@ interface ProfileCardProps {
   comments: Comment[];
 }
 
-const ProfileCard = ({
-  listName,
-  username,
-  description,
-  likeCount,
-  starCount,
-  tags,
-  comments
-}: ProfileCardProps) => {
+const ProfileCard = () => {
+
+  // 더미 데이터
+  const bookmarkInfo: BookmarkListProps = {
+    listName: "북마크리스트",
+    email: "abc@gmail.com",
+    description: "인기 북마크 리스트",
+    likeCount: 200,
+    starCount: 300,
+    tags: [
+      { id: 1, text: "#태그" },
+      { id: 2, text: "#태그2" },
+    ],
+    comments: [
+      { id: 1, text: "좋아요~1" },
+      { id: 2, text: "좋아요~2" },
+    ]
+  }
+
   return (
     <div className="bg-aside-layout rounded-3xl w-80 p-8 flex flex-col items-center">
       {/* 더보기 버튼 */}
-      <div className="self-end">
-        <button className="text-center">...</button>
-      </div>
+      <details className="dropdown dropdown-end dropdown-bottom self-end">
+        <summary className="btn m-1 bg-aside-layout">
+          <VscKebabVertical />
+        </summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2">
+          <li><a className="justify-center">수정하기</a></li>
+          <li><a className="justify-center">삭제하기</a></li>
+          <li><a className="justify-center">url 복사</a></li>
+          <li><a className="justify-center">신고하기</a></li>
+        </ul>
+      </details>
       {/* 북마크리스트 썸네일 */}
       <div className="mb-4">
         <img src={profileImage} className="w-16 h-16 bg-gray-200 rounded-full mb-4" />
       </div>
-      <h2 className="text-xl font-bold mb-1">{listName}</h2>
-      <p className="text-gray-500 mb-2">@{username}</p>
-      <p className="text-center text-sm mb-6">{description}</p>
+      <h2 className="text-xl font-bold mb-1">{bookmarkInfo.listName}</h2>
+      <p className="text-gray-500 mb-2">{bookmarkInfo.email}</p>
+      <p className="text-center text-sm mb-6">{bookmarkInfo.description}</p>
       {/* 좋아요, 즐겨찾기 칸 */}
       <div className="grid grid-cols-6">
         <div className="col-start-2 flex">
           <div>
-            <img src={heart} className="text-gray-500 mr-1" />
+            <FaRegHeart />
           </div>
           <div>
-            <span>{likeCount}</span>
+            <span>{bookmarkInfo.likeCount}</span>
           </div>
         </div>
         <div className="col-start-5 flex">
           <div>
-            <img src={star} className="text-gray-500 mr-1" />
+            <FaRegStar />
           </div>
           <div>
-            <span>{starCount}</span>
+            <span>{bookmarkInfo.starCount}</span>
           </div>
         </div>
       </div>
       {/* 태그 창 */}
-      <div className="m-6">
-        {tags.map((tag) => (
-          <p>{tag.text} </p>
+      <div className="m-6 flex">
+        {bookmarkInfo.tags.map((tag) => (
+          <p>{tag.text}&nbsp;</p>
         ))}
       </div>
       <div className="flex flex-col">
@@ -79,7 +96,7 @@ const ProfileCard = ({
         </div>
         <div className="grid grid-cols-8 w-full">
           <div className="col-start-2 col-span-6 max-h-20 overflow-y-auto">
-            {comments.map((comment) => (
+            {bookmarkInfo.comments.map((comment) => (
               <div key={comment.id} className="flex items-center mb-2">
                 <div className="w-2 h-2 bg-blue-500 rounded-full mr-2"></div>
                 <p className="text-sm">{comment.text}</p>

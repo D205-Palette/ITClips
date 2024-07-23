fc49a1ea136d51f896bbc2ca235b85490432e84f
김세진 <rlatpwls30@naver.com>
2024-07-23 17:23:57 +0900

    Test : AsideProfile.tsx 테스트 코드 수정

--- client/itclips/src/components/aside/AsideProfile.tsx
+++ client/itclips/src/components/aside/AsideProfile.tsx
@@ -1,4 +1,5 @@
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
 import { useState } from "react";
 
 import ImageContainer from "./items/ImageContainer";
@@ -10,7 +11,7 @@ const AsideProfile = () => {
   // (임시) 팔로우 상태인지?
   const [ isFollow, setIsFollow ] = useState(false);
   // (임시) 다른 유저의 정보인지?
-  const [ isOther, setIsOther ] = useState(true);
+  const [ isOther, setIsOther ] = useState(false);
 
   // 채팅하기 버튼을 눌렀을 때
   const onClickStartChat = () => {
@@ -24,10 +25,14 @@ const AsideProfile = () => {
 
   return (
     <div className="bg-aside-layout rounded-3xl w-80 p-8 flex flex-col items-center">
-      {/* 다른 유저일때 채팅하기 버튼 활성화 */}
-      { isOther && (
+      {/* 다른 유저일때 채팅하기 버튼 또는 환경설정 활성화 */}
+      { isOther ? (
         <button className="btn btn-ghost btn-circle ms-16">
             <IoChatboxEllipsesOutline className="h-8 w-8" />
+        </button>
+      ) : (
+        <button className="btn btn-ghost btn-circle ms-16">
+            <IoSettingsOutline className="h-6 w-6" />
         </button>
       )}
       {/* 프로필 이미지 컨테이너: 안에 이미지 이름을 넘겨주면 이미지 출력 */}

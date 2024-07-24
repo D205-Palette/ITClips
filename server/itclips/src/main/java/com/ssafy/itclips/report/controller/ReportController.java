dcac9adb3bb3fc5ec317c361669189b435f4c952
정진규 <wlsrb6905@naver.com>
2024-07-24 17:00:01 +0900

    Fix : 신고 컨트롤러 swagger

--- server/itclips/src/main/java/com/ssafy/itclips/report/controller/ReportController.java
+++ server/itclips/src/main/java/com/ssafy/itclips/report/controller/ReportController.java
@@ -2,6 +2,9 @@ package com.ssafy.itclips.report.controller;
 
 import com.ssafy.itclips.report.dto.ReportDTO;
 import com.ssafy.itclips.report.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +19,13 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping("/bookmark/{userId}/{bookmarkId}")
+    @Operation(summary = "북마크 신고", description = "북마크를 신고합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "북마크 신고가 성공적으로 진행되었습니다."),
+            @ApiResponse(responseCode = "400", description = "이미 신고 한 북마크입니다."),
+            @ApiResponse(responseCode = "404", description = "유저, 북마크를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류가 발생했습니다.")
+    })
     public ResponseEntity<?> reportBookmark(@PathVariable Long userId,
                                             @PathVariable Long bookmarkId,
                                             @RequestBody ReportDTO reportDTO) {
@@ -24,6 +34,13 @@ public class ReportController {
     }
 
     @PostMapping("/list/{userId}/{listId}")
+    @Operation(summary = "북마크 리스트 신고", description = "북마크 리스트를 신고합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "북마크 리스트 신고가 성공적으로 진행되었습니다."),
+            @ApiResponse(responseCode = "400", description = "이미 신고 한 북마크 리스트입니다."),
+            @ApiResponse(responseCode = "404", description = "유저, 북마크리스트를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류가 발생했습니다.")
+    })
     public ResponseEntity<?> reportBookmarkList(@PathVariable Long userId,
                                             @PathVariable Long listId,
                                             @RequestBody ReportDTO reportDTO) {

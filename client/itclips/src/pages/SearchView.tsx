52d6a260a2a9a813f1aa472d18814fdfce38c81d
김세진 <rlatpwls30@naver.com>
2024-07-25 16:27:48 +0900

    Feat : search 검색 페이지 레이아웃 조정 및 검색바 구현

--- client/itclips/src/pages/SearchView.tsx
+++ client/itclips/src/pages/SearchView.tsx
@@ -3,18 +3,33 @@ import React, { useState } from 'react';
 // components
 import MessageLayout from "../components/aside/MessageLayout";
 import SearchBar from "../components/search/SearchBar";
-import RecommendedItems from '../components/search/RecommandedItems';
 import RealtimeList from "../components/search/RealtimeList";
-
-// icons
-import { FaList } from "react-icons/fa";
+import SearchMain from "../components/search/SearchMain";
+import SearchUser from "../components/search/SearchUser";
+import SearchBookmarkList from "../components/search/SearchBookmarkList";
+import SearchRoadmap from "../components/search/SearchRoadmap.tsx";
+import SearchTag from "../components/search/SearchTag";
 
 // stores
 import { asideStore } from "../stores/asideStore";
 
-const About: React.FC = () => {
+interface SearchInfo {
+  category: string;
+  keyword: string;
+}
+
+const SearchView = () => {
+
   const isMessageOpen = asideStore(state => state.isMessageOpen);
-  const [ viewMode, setViewMode ] = useState<'grid' | 'list'>('grid');
+  const [ whatCategory, setWhatCategory ] = useState("카테고리");
+  const [ keyword, setKeyword ] = useState("");
+
+  const selectCategory = (category: string, keyword: string) => {
+    setWhatCategory(category);
+    setKeyword(keyword);
+  }
+
+  // 검색바에서 어떤 카테고리 골랐는지에 따라 검색 메인에 출력되는 컴포넌트 다르게
 
   return (
     <>
@@ -30,27 +45,27 @@ const About: React.FC = () => {
         <div id="Main" className="lg:col-start-3 lg:col-span-4 md:col-start-2 md:col-span-5 sm:col-start-2 sm:col-span-6">
           <div className="container mx-auto p-4">
             <div className=" md:flex-row justify-between gap-4 mb-4">
+              {/* 검색 바 */}
               <div className="w-full">
-                <SearchBar />
+                <SearchBar handleCategory={selectCategory} />
               </div>
-              <div className="mt-4">
-                <div className="flex justify-end mb-4">
-                  <button className="btn btn-sm mr-2" onClick={() => setViewMode('grid')}>Grid</button>
-                  <button className="btn btn-sm btn-ghost" onClick={() => setViewMode('list')}>
-                    <FaList />
-                  </button>
-                </div>
-                <RecommendedItems />
-              </div>
-            </div>
-            <div className="w-1/4">
-              <RealtimeList />
+              {/* 카테고리 조건에 따라 검색 컴포넌트 변경 */}
+              {/* 검색 버튼을 눌렀을 때 적용되게 */}
+              { whatCategory === "카테고리" && <SearchMain /> }
+              { whatCategory === "유저" && <SearchUser /> }
+              { whatCategory === "북마크리스트" && <SearchBookmarkList /> }
+              { whatCategory === "로드맵" && <SearchRoadmap /> }
+              { whatCategory === "태그" && <SearchTag /> }
             </div>
           </div>
+        </div>
+        {/* 실시간 인기 순위는 검색 메인에서만 */}
+        <div className="w-60 col-start-7">
+          <RealtimeList />
         </div>
       </div>
     </>
   );
 };
 
-export default About;
+export default SearchView;

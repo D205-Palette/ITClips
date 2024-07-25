803cb7371fc32cf8fe216278b36572a83b6fb3a2
김세진 <rlatpwls30@naver.com>
2024-07-25 16:32:53 +0900

    Feat : search 유저 검색 화면 구현

---
+++ client/itclips/src/components/search/SearchUser.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+// components
+import SearchUserItem from "./SearchUserItem";
+
+const SearchUser = () => {
+
+  // 더미 데이터
+  const users = [
+    { username: "고양양", email: "abc@gmail.com", followers: 100, following: 20, listCount: 5, roadmapCount: 2 },
+    { username: "고양양", email: "abc@gmail.com", followers: 100, following: 20, listCount: 5, roadmapCount: 2 },
+    { username: "고양양", email: "abc@gmail.com", followers: 100, following: 20, listCount: 5, roadmapCount: 2 },
+    { username: "고양양", email: "abc@gmail.com", followers: 100, following: 20, listCount: 5, roadmapCount: 2 },
+    { username: "고양양", email: "abc@gmail.com", followers: 100, following: 20, listCount: 5, roadmapCount: 2 },
+  ]
+
+  // 프로필 카드 클릭했을 때 사용자 프로필로 이동
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
+        {users.map((user, index) => (
+          <div>
+            <SearchUserItem
+              key={index}
+              username={user.username}
+              email={user.email}
+              followers={user.followers}
+              following={user.following}
+              listCount={user.listCount}
+              roadmapCount={user.roadmapCount}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchUser;

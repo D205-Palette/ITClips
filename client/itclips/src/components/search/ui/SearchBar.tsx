0c9c758f9d623ada143e5101e3f933e2ed5a148f
김세진 <rlatpwls30@naver.com>
2024-07-26 10:04:10 +0900

    Fix : search 검색바 카테고리 드롭다운 버튼 메세지창 위로 출력되는 에러 수정

--- client/itclips/src/components/search/ui/SearchBar.tsx
+++ client/itclips/src/components/search/ui/SearchBar.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+// components
+import SearchCategoryDropdown from "./SearchCategoryDropdown";
+
 interface ChildProps {
   handleCategory: (category: string, keyword: string) => void;
 }
@@ -8,21 +11,16 @@ const SearchBar: React.FC<ChildProps> = ({ handleCategory }) => {
   const [ searchCategory, setSearchCategory ] = useState("카테고리");
   const [ searchKeyword, setSearchKeyword ] = useState("");
 
-  const categories = ["카테고리", "유저", "북마크리스트", "로드맵", "태그"];
+  // const categories = ["카테고리", "유저", "북마크리스트", "로드맵", "태그"];
+
+  const selectCategory = (category: string) => {
+    setSearchCategory(category);
+  };
 
   return (
     <div className="flex items-center space-x-2">
-      <div className="dropdown">
-        <label tabIndex={0} className="btn m-1 w-32">{searchCategory}</label>
-        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-40">
-          {/* 검색 카테고리 드롭다운 */}
-          {categories.map((category) => (
-            <li key={category} onClick={() => setSearchCategory(category)} className="self-center">
-              <a>{category}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* 검색 카테고리 드롭다운 */}
+      <SearchCategoryDropdown selectCategory={selectCategory} />
       {/* 검색바 */}
       <input 
         type="text" 

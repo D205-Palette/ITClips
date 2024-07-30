dbc2b7e323c33bb3c9ad8b98f3af149b96aaacaf
김세진 <rlatpwls30@naver.com>
2024-07-30 15:45:24 +0900

    Design : aside 메세지창 바탕색 변경

--- client/itclips/src/components/aside/layout/MessageContainer.tsx
+++ client/itclips/src/components/aside/layout/MessageContainer.tsx
@@ -17,7 +17,7 @@ const MessageContainer: React.FC<MessageContainerProps> = ({ messages }) => {
     <div className="flex-1 overflow-y-auto mb-4 space-y-4">
       {messages.map((message: Message) => (
         <div key={message.id} className={`flex ${message.isSent ? 'justify-end' : 'justify-start'}`}>
-          <div className={`max-w-xs p-3 rounded-lg ${message.isSent ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>
+          <div className={`max-w-xs p-3 rounded-lg text-black ${message.isSent ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>
             {message.content}
           </div>
         </div>

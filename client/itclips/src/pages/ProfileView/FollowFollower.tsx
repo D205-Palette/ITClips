5fd7fcede06d9047212344e7059fa518e669bcce
김동준 <kdj4355@naver.com>
2024-07-30 12:24:06 +0900

    Fix : merge

--- client/itclips/src/pages/ProfileView/FollowFollower.tsx
+++ client/itclips/src/pages/ProfileView/FollowFollower.tsx
@@ -1,43 +1,10 @@
 import SearchBar from "../../components/main/MainSearchBar";
 import MainTab from "../../components/main/MainTab";
 import FollowerList from '../../components/follow/FollowerList'
-import FollowItem from '../../components/follow/FollowItem'
-
-const users = [
-    {
-        pk: 1,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양친구",
-      },
-      {
-        pk: 2,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양친친구",
-      },
-      {
-        pk: 3,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양고양친구",
-      },
-]
-
 const Follower = () => {       
   return (
     <>
-    {users.map((user) =><FollowItem user={user} isFollower={true} /> )}
-    
+    <FollowerList />
     </>
   );
 };
--- client/itclips/src/pages/ProfileView/FollowFollowing.tsx
+++ client/itclips/src/pages/ProfileView/FollowFollowing.tsx
@@ -1,41 +1,10 @@
 import SearchBar from "../../components/main/MainSearchBar";
 import MainTab from "../../components/main/MainTab";
-import FollowItem from "../../components/follow/FollowItem";
-
-const users = [
-    {
-        pk: 1,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양친구",
-      },
-      {
-        pk: 2,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양친친구",
-      },
-      {
-        pk: 3,
-        image: "/profile_image.png",
-        tags:[
-            "관심사 1",
-            "관심사 2"
-        ],
-        name: "고양고양친구",
-      },
-]
-
+import FollowingList from "../../components/follow/FollowingList";
 const Following = () => {       
   return (
     <>
-        {users.map((user) =><FollowItem user={user} isFollower={false} /> )}
+        <FollowingList />
     </>
   );
 };

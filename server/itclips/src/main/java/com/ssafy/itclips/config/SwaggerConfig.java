cedb4b368ab8f9b9b5e05e67204ddc968ee5ec87
정진규 <wlsrb6905@naver.com>
2024-07-31 13:56:24 +0900

    Fix : swagger config

--- server/itclips/src/main/java/com/ssafy/itclips/config/SwaggerConfig.java
+++ server/itclips/src/main/java/com/ssafy/itclips/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.ssafy.itclips.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +14,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
-                .components(new Components().addSecuritySchemes("basicScheme",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .components(new Components())
                 .info(apiInfo());
     }
 

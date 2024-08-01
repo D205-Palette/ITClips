1b133f1bebf62db5b4589d918779ad5345266676
김세진 <rlatpwls30@naver.com>
2024-08-01 17:59:57 +0900

    Refactor : aside 팔로우 관리 버튼 NavLink 주소 수정

--- client/itclips/src/components/aside/layout/UserActivityInfo.tsx
+++ client/itclips/src/components/aside/layout/UserActivityInfo.tsx
@@ -1,6 +1,6 @@
 // UserActivityInfo.tsx 는 유저의 정보(팔로워, 팔로잉, 리스트 갯수, 북마크 갯수)를 출력하고
 // 팔로워, 팔로잉의 숫자를 누르면 팔로우 관리 창으로 이동하는 컴포넌트
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { NavLink } from "react-router-dom";
 
@@ -12,7 +12,8 @@ interface ActivityInfo {
 }
 
 const UserActivityInfo = () => {
-  const navigate = useNavigate()
+  
+  const params = useParams();
 
   // 더미 데이터
   const data: ActivityInfo = {
@@ -27,7 +28,7 @@ const UserActivityInfo = () => {
       <div className="grid grid-cols-12">
         <div className="col-start-3 col-span-5 text-start text-gray-500">팔로워</div>
         <div className="col-start-9">
-          <NavLink to="/user/:user_id/follow/follower">
+          <NavLink to={`/user/${params.user_id}/follow/follower`}>
             <button id="followers" className="text-start font-bold hover:bg-sky-100 hover:rounded">{data.followers}</button>
           </NavLink>
         </div>
@@ -35,7 +36,7 @@ const UserActivityInfo = () => {
       <div className="grid grid-cols-12">
         <div className="col-start-3 col-span-5 text-start text-gray-500">팔로잉</div>
         <div className="col-start-9">
-          <NavLink to="/user/:user_id/follow/following">
+          <NavLink to={`/user/${params.user_id}/follow/following`}>
             <button id="following" className="text-start font-bold hover:bg-sky-100 hover:rounded">{data.following}</button>
           </NavLink>
         </div>

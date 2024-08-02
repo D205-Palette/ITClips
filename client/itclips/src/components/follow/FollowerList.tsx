20b5088b791ed9ff5d290582a138272a973ec416
김세진 <rlatpwls30@naver.com>
2024-08-02 10:50:54 +0900

    Feat : follow 인터페이스 변경 및 팔로우 목록 조회 기능 추가

--- client/itclips/src/components/follow/FollowerList.tsx
+++ client/itclips/src/components/follow/FollowerList.tsx
@@ -1,32 +1,50 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // components
 import FollowerItem from "./FollowerItem";
 
-interface User {
+// apis
+import { getFollowingList } from "../../api/followApi";
+
+// stores
+import { authStore } from "../../stores/authStore";
+
+interface Follower {
   id: number;
-  username: string;
-  imageUrl: string;
+  fromUserId: number;
+  toUserId: number;
+  nickname: string;
+  profileImage: string;
   email: string;
-  tag: string;
 }
 
 const FollowerList = () => {
 
-  const params = useParams();
+  const [ followingList, setFollowingList ] = useState<Follower[]>([]);
+  const { user_id } = useParams<{ user_id: string }>();
+  const token = authStore(state => state.token);
 
-  // 더미 데이터
-  const data: User[] = [
-    { id: 1, username: "고양양", imageUrl: "", email: "abc@gmail.com", tag: "#관심사1 #관심사2 #태그" },
-    { id: 2, username: "고양양", imageUrl: "", email: "abc@gmail.com", tag: "#관심사1 #관심사2 #태그" },
-    { id: 3, username: "고양양", imageUrl: "", email: "abc@gmail.com", tag: "#관심사1 #관심사2 #태그" },
-    { id: 4, username: "고양양", imageUrl: "", email: "abc@gmail.com", tag: "#관심사1 #관심사2 #태그"},
-  ]
+  // 팔로우 목록 조회
+  useEffect(() => {
+    const fetchFollowingList = async () => {
+      if (user_id) {
+        try {
+          const response = await getFollowingList(parseInt(user_id, 10), token);
+          setFollowingList(response.data);
+          console.log(response);
+        } catch (error) {
+          console.error("팔로우 목록 조회 실패", error);
+        }
+      }
+    };
+    fetchFollowingList();
+  }, [user_id, token]);
 
   return (
     <div className="mt-4">
       {/* 추천 결과 */}
-      <FollowerItem items={data} />
+      <FollowerItem items={followingList} />
     </div>
   );
 };

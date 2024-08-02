3a81b49f340958b90f88395739a9e36942456513
김세진 <rlatpwls30@naver.com>
2024-08-02 23:07:46 +0900

    Feat : aside 유저 정보(닉넴, 이메일, 소개글) api 연결 구현

--- client/itclips/src/components/aside/AsideProfile.tsx
+++ client/itclips/src/components/aside/AsideProfile.tsx
@@ -1,6 +1,9 @@
 // AsideProfile.tsx 는 나의 프로필 및 다른 유저의 프로필을 보여주는 컴포넌트
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+// apis
+import { checkUserInfo } from "../../api/authApi";
 
 // icons
 import { IoChatboxEllipsesOutline, IoSettingsOutline } from "react-icons/io5";
@@ -16,30 +19,42 @@ import darkModeStore from "../../stores/darkModeStore";
 import { authStore } from "../../stores/authStore";
 
 interface User {
-  username: string;
-  email: string;
-  description: string;
-  likeCount: number;
-  starCount: number;
+  id?: number;
+  nickname?: string;
+  job?: string;
+  bio?: string;
+  gender?: boolean;
+  email?: string;
+  darkMode?: boolean;
+  birth?: string;
 }
 
 const AsideProfile = () => {
 
-  // 더미 데이터
-  const UserInfo: User = {
-    username: "고양양",
-    email: "abc@gmail.com",
-    description: "안녕하세요 고양양입니다. 재미있는 IT정보를 클립할거에여~",
-    likeCount: 200,
-    starCount: 300,
-  }
-
   // 내 정보 가져오기
-  const myUserId = authStore(state => state.userId);
+  const myInfo = authStore(state => state.userInfo);
 
   // url에서 user_id 가져오기
   const params = useParams<{ user_id?: string }>();
   const urlUserId = params.user_id ? parseInt(params.user_id, 10) : undefined;
+  
+  const [ urlUserInfo, setUrlUserInfo ] = useState<User>();
+
+  // 팔로우 목록 조회
+  useEffect(() => {
+    const fetchFollowingList = async () => {
+      if (urlUserId) {
+        try {
+          const response = await checkUserInfo(urlUserId);
+          setUrlUserInfo(response.data);
+          console.log(response);
+        } catch (error) {
+          console.error("유저 조회 실패", error);
+        }
+      }
+    };
+    fetchFollowingList();
+  }, [urlUserId]);
 
   const isDark = darkModeStore(state => state.isDark);
 
@@ -73,7 +88,7 @@ const AsideProfile = () => {
   return (
     <div className={`${ isDark ? "bg-base-300" : "bg-sky-100" } rounded-3xl w-80 p-8 flex flex-col items-center`}>
       {/* 다른 유저일때 채팅하기 버튼 또는 환경설정 활성화 */}
-      {myUserId !== urlUserId ? (
+      {myInfo.id !== urlUserId ? (
         <button className="btn btn-ghost btn-circle ms-16" onClick={onClickStartChat}>
           <IoChatboxEllipsesOutline className="h-8 w-8" />
         </button>
@@ -85,9 +100,9 @@ const AsideProfile = () => {
       {/* 프로필 이미지 컨테이너 */}
       <ImageContainer />
       {/* 닉네임, 이메일, 소개글 정보 컨테이너 */}
-      <UserDetailInfo {...UserInfo} />
+      {myInfo && <UserDetailInfo {...urlUserInfo} />}
       {/* 자기인지 아닌지에 따라 활성화되는 팔로우 버튼 */}
-      {myUserId !== urlUserId ? (
+      {myInfo.id !== urlUserId ? (
         <button
           className={`text-white btn ${isFollow ? 'btn-error' : 'btn-info'}`}
           onClick={onClickFollow}

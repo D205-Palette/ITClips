7cca6cbe816cf2e11e68dd273767ffa47acd9219
김세진 <rlatpwls30@naver.com>
2024-08-02 17:24:18 +0900

    Fix : localStorage token 가져오는거 수정(근데 안됨)

--- client/itclips/src/api/apiUtils.ts
+++ client/itclips/src/api/apiUtils.ts
@@ -1,18 +1,30 @@
 // apiUtils.ts
 import api from "./api";
-import { authStore } from "../stores/authStore";
-const { token } = authStore()
+// const { token } = authStore();
+
+// 처음에 토큰 못가져오는거 임시로 수정 (작동안됨)
+const authStoreJSON = localStorage.getItem("authStore");
+const authStore = authStoreJSON ? JSON.parse(authStoreJSON) : {};
+const token = authStore.token || null;
 
 // 인증된 요청을 위한 유틸리티 함수
 export const authenticatedRequest = (method: string, url: string, data?: any, params?: any) => {
+
+  // 토큰 다시 가져오기(작동안됨)
+  const headers: Record<string, string> = {};
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
   return api({
     method,
     url,
     data,
     params,
-    headers : {
-       Authorization: `Bearer ${token}`
-    }
+    headers,
+    // headers: {
+    //   Authorization: `Bearer ${token}`
+    // }
   });
 };
 

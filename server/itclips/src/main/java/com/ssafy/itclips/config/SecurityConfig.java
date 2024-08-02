25ca88567c0712f6ff6f2c456bace9203645fd13
정진규 <wlsrb6905@naver.com>
2024-08-02 12:42:53 +0900

    Fix : security cors

--- server/itclips/src/main/java/com/ssafy/itclips/config/SecurityConfig.java
+++ server/itclips/src/main/java/com/ssafy/itclips/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("https://localhost:3000");
         configuration.addAllowedOrigin("http://localhost:5500");
         configuration.addAllowedOrigin("https://localhost");
+        configuration.addAllowedOrigin("https://i11d205.p.ssafy.io");  // 추가된 출처
 //        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");

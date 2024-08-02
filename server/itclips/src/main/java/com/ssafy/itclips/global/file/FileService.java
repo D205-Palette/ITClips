234fe5e774e25e4f82f0a37f2706e9590eef3f0f
정진규 <wlsrb6905@naver.com>
2024-08-02 16:11:10 +0900

    Chore : s3 설정

---
+++ server/itclips/src/main/java/com/ssafy/itclips/global/file/FileService.java
@@ -0,0 +1,9 @@
+package com.ssafy.itclips.global.file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+}

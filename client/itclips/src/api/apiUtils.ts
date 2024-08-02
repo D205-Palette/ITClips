c8502b0a6090d693ac9c5c2dde3bd25e621b14f7
김세진 <rlatpwls30@naver.com>
2024-08-02 20:47:36 +0900

    Refactor : apiUtils.ts 필요없는 코드 삭제

--- client/itclips/src/api/apiUtils.ts
+++ client/itclips/src/api/apiUtils.ts
@@ -3,7 +3,6 @@ import api from "./api";
 
 // 인증된 요청을 위한 유틸리티 함수
 export const authenticatedRequest = (method: string, url: string, data?: any, params?: any) => {
-
   return api({
     method,
     url,

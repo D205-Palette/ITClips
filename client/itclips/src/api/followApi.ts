b2141649ae07a242ac35df6da850f6fb4d3d9472
김세진 <rlatpwls30@naver.com>
2024-08-03 16:12:45 +0900

    Feat : follow followApi.ts에 팔로우 관련 api 로직 구현

--- client/itclips/src/api/followApi.ts
+++ client/itclips/src/api/followApi.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { API_BASE_URL } from "../config";
 import { authenticatedRequest } from "./apiUtils";
 
 // 팔로잉 목록 조회
@@ -17,27 +15,18 @@ export const getFollowCounts = (userId: number) => {
   return authenticatedRequest("get", "/follow/count", undefined, { userId })
 }
 
-// 팔로우 (팔로잉 추가) - 수정필요
-export const follow = () => {
-  return axios({
-    method: "post",
-    url: `${API_BASE_URL}/follow/follow`
-  })
+// 팔로우 (팔로잉 추가)
+export const follow = (fromUserId: number, toUserId: number) => {
+  return authenticatedRequest("post", "/follow/follow", undefined, { fromUserId, toUserId })
 }
 
 
-// 팔로잉 취소 (팔로잉 삭제) - 수정필요
-export const unfollow = () => {
-  return axios({
-    method: "delete",
-    url: `${API_BASE_URL}/follow/unfollow`
-  })
+// 팔로잉 취소 (팔로잉 삭제)
+export const unfollow = (fromUserId: number, toUserId: number) => {
+  return authenticatedRequest("post", "/follow/unfollow", undefined, { fromUserId, toUserId })
 }
 
 // 팔로워 삭제 - 수정필요
-export const removeFollower = () => {
-  return axios({
-    method: "delete",
-    url: `${API_BASE_URL}/follow/follower`
-  })
+export const removeFollower = (fromUserId: number, toUserId: number) => {
+  return authenticatedRequest("delete", "/follow/unfollow", undefined, { fromUserId, toUserId })
 }

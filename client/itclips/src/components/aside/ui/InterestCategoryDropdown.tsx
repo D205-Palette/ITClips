97f815580ad2c5ce2c18d41e2579dab205ea72e0
김세진 <rlatpwls30@naver.com>
2024-08-03 22:10:20 +0900

    Feat : 관심사 카테고리 받아온거 골랐을 때 관심사 id 넘겨주도록 구현

--- client/itclips/src/components/aside/ui/InterestCategoryDropdown.tsx
+++ client/itclips/src/components/aside/ui/InterestCategoryDropdown.tsx
@@ -1,28 +1,27 @@
 import React, { useState, useRef, useEffect } from "react";
-
-// stores
 import darkModeStore from "../../../stores/darkModeStore";
-
-// apis
 import { getInterestTag } from "../../../api/tagApi";
 
+interface InterestCategory {
+  id: number;
+  title: string;
+}
+
 interface InterestProps {
-  selectCategory: (category: string) => void;
+  selectCategory: (category: InterestCategory) => void;
 }
 
 const InterestCategoryDropdown: React.FC<InterestProps> = ({ selectCategory }) => {
   const [searchCategory, setSearchCategory] = useState<string>("관심사 선택");
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [categories, setCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<InterestCategory[]>([]);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const isDark = darkModeStore(state => state.isDark);
 
-  // 카테고리 목록 가져오기
   const fetchCategories = async () => {
     try {
       const response = await getInterestTag();
-      const titles = response.data.map((item: { title: string }) => item.title);
-      setCategories(titles);
+      setCategories(response.data);
     } catch (error) {
       console.error("Failed to fetch categories:", error);
     }
@@ -32,17 +31,14 @@ const InterestCategoryDropdown: React.FC<InterestProps> = ({ selectCategory }) =
     fetchCategories();
   }, []);
 
-  // 드롭다운 메뉴가 열렸는지 확인
   const toggleDropdown = (): void => setIsOpen(!isOpen);
 
-  // 카테고리 제어
-  const handleCategory = (category: string) => {
+  const handleCategory = (category: InterestCategory) => {
     selectCategory(category);
-    setSearchCategory(category);
+    setSearchCategory(category.title);
     setIsOpen(false);
   }
 
-  // 마우스 클릭을 메뉴 바깥에서 할 경우 자동으로 닫히게
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
@@ -71,9 +67,9 @@ const InterestCategoryDropdown: React.FC<InterestProps> = ({ selectCategory }) =
         <div className="absolute left-0 bottom-full mb-1 bg-base-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 max-h-60 overflow-y-auto">
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
             {categories.map((category) => (
-              <li key={category} onClick={() => handleCategory(category)}>
+              <li key={category.id} onClick={() => handleCategory(category)}>
                 <div className={`${isDark && "text-gray-200"} text-center block px-4 py-2 hover:bg-gray-100`}>
-                  {category}
+                  {category.title}
                 </div>
               </li>
             ))}

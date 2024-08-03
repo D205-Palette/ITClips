d5d7c371ed73d4aa6d54c4c133e7f2ff56971b9c
김민솔 <artkms4815@naver.com>
2024-08-03 17:34:25 +0900

    ㄹㄷ

--- server/itclips/src/main/java/com/ssafy/itclips/feed/repository/FeedRepository.java
+++ server/itclips/src/main/java/com/ssafy/itclips/feed/repository/FeedRepository.java
@@ -7,17 +7,40 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @RequiredArgsConstructor
 @Repository
 public class FeedRepository {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    HashOperations<String, String, String> hashOps;
+    // 피드 정보 담을 객체 "bookmarkFeed / roadmapFeed", userId, roadmapId
+    HashOperations<String, Long, List<Long>> hashOps;
 
     @PostConstruct
     private void init() {
-        hashOps = redisTemplate.opsForHash();
+        hashOps= redisTemplate.opsForHash();
+
     }
 
+    //유저 로드맵 피드
+    public List<Long> getUserRoadmapFeed(Long userId) {
+        return hashOps.get("roadmapFeed",userId);
+    }
+
+    //로드맵 저장
+    public void saveRoadmapFeed(Long userId, Long roadmapId) {
+        List<Long> ids = hashOps.get("roadmapFeed",userId);
+
+        if(ids==null){
+            ids = new ArrayList<>();
+        }
+        ids.add(roadmapId);
+        hashOps.put("roadmapFeed",userId,ids);
+
+        redisTemplate.expire("roadmapFeed", 7, TimeUnit.DAYS);
+    }
 }
--- server/itclips/src/main/java/com/ssafy/itclips/feed/service/FeedServiceImpl.java
+++ server/itclips/src/main/java/com/ssafy/itclips/feed/service/FeedServiceImpl.java
@@ -1,15 +1,20 @@
 package com.ssafy.itclips.feed.service;
 
+import com.ssafy.itclips.error.CustomException;
+import com.ssafy.itclips.error.ErrorCode;
 import com.ssafy.itclips.feed.repository.FeedRepository;
+import com.ssafy.itclips.follow.entity.Follow;
+import com.ssafy.itclips.follow.repository.FollowRepository;
 import com.ssafy.itclips.roadmap.dto.RoadmapInfoDTO;
-import jakarta.annotation.PostConstruct;
+import com.ssafy.itclips.roadmap.entity.Roadmap;
+import com.ssafy.itclips.roadmap.repository.RoadmapLikeRepository;
+import com.ssafy.itclips.roadmap.repository.RoadmapRepository;
+import com.ssafy.itclips.roadmap.repository.RoadmapStepRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.data.redis.core.HashOperations;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,16 +23,44 @@ import java.util.List;
 public class FeedServiceImpl implements FeedService{
 
     private final FeedRepository feedRepository;
+    private final RoadmapRepository roadmapRepository;
+    private final RoadmapStepRepository roadmapStepRepository;
+    private final RoadmapLikeRepository roadmapLikeRepository;
+    private final FollowRepository followRepository;
 
     // 로드맵 피드 출력
     @Override
     public List<RoadmapInfoDTO> getRoadmapFeed(Long userId) {
-        return List.of();
+        List<Long> roadmapIds = feedRepository.getUserRoadmapFeed(userId);
+        List<RoadmapInfoDTO> roadmapInfoDTOList = new ArrayList<>();
+
+        if(roadmapIds != null) {
+            for (Long roadmapId : roadmapIds) {
+                Roadmap roadmap = roadmapRepository.findById(roadmapId)
+                        .orElseThrow(()-> new CustomException(ErrorCode.ROADMAP_NOT_FOUND));
+
+                // 로드맵 단계 수
+                Long stepCnt = roadmapStepRepository.countByRoadmapId(roadmap.getId());
+                // 체크한단계 수
+                Long checkCnt = roadmapStepRepository.countByRoadmapIdAndCheck(roadmap.getId(), true);
+                // 좋아요 수
+                Long likeCnt = roadmapLikeRepository.countByRoadmapId(roadmap.getId());
+
+                RoadmapInfoDTO roadmapInfoDTO = RoadmapInfoDTO.toDto(roadmap,stepCnt,checkCnt,likeCnt);
+                roadmapInfoDTOList.add(roadmapInfoDTO);
+            }
+
+        }
+        return roadmapInfoDTOList;
     }
 
     // 로드맵 피드 저장
     @Override
     public void saveRoadmapFeed(Long userId, Long roadmapId) {
+        List<Follow> followersList = followRepository.findByToId(userId);
 
+        for(Follow follow : followersList) {
+            feedRepository.saveRoadmapFeed(follow.getFrom().getId(),roadmapId);
+        }
     }
 }

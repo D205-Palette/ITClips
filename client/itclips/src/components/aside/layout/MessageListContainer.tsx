66124082c8a0d115fcdb59ff6e71c3f33b093dee
김세진 <rlatpwls30@naver.com>
2024-08-04 21:10:41 +0900

    Refactor : MessageListContainer 인터페이스 이름 변경

--- client/itclips/src/components/aside/layout/MessageListContainer.tsx
+++ client/itclips/src/components/aside/layout/MessageListContainer.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-interface Message {
+interface ChatRoom {
   id: number;
   title: string;
   subtitle: string;
@@ -10,7 +10,7 @@ interface Message {
 }
 
 interface ChildComponentProps {
-  messages: Message[];
+  messages: ChatRoom[];
   onClickMessage: (id: number) => any;
 }
 
@@ -18,7 +18,7 @@ const MessageContainer: React.FC<ChildComponentProps> = ({ messages, onClickMess
   return (
     <div>
       <ul className="space-y-4 h-[26rem] overflow-y-auto">
-        {messages.map((message: Message) => (
+        {messages.map((message: ChatRoom) => (
           <li key={message.id} className="flex justify-between items-center bg-base-100 rounded w-100 px-3 py-2" onClick={() => onClickMessage(message.id)}>
             <div className="p-3">
               <h3 className="font-semibold text-start">{message.title}</h3>

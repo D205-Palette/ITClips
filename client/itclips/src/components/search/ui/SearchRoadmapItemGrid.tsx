b76d645779342776de62b5d62f6fee73a15adbc6
김세진 <rlatpwls30@naver.com>
2024-08-04 23:18:37 +0900

    Refactor : 로드맵 검색 결과 Grid 더보기 메뉴 추가 및 NavLink 주소 수정

--- client/itclips/src/components/search/ui/SearchRoadmapItemGrid.tsx
+++ client/itclips/src/components/search/ui/SearchRoadmapItemGrid.tsx
@@ -30,12 +30,17 @@ const SearchRoadmapItemGrid: React.FC<RoadmapProps> = ({ item }) => {
   };
 
   return (
-    <div>
+    <div className="relative">
       <NavLink
-        to={`/bookmarklist/${item.id}`}
+        to={`/roadmap/${item.id}`}
         className="block h-full"
       >
-        <img src={image} alt={item.title} className="w-full h-40 object-cover" />
+        <div className="relative">
+          <img src={image} alt={item.title} className="w-full h-40 object-cover" />
+          <div className="absolute top-2 right-2 z-10" onClick={handleNavLink}>
+            <SearchItemKebabDropdown />
+          </div>
+        </div>
         <div className="p-3">
           <div className="flex flex-col">
             <h3 className="text-lg font-semibold truncate mb-1">{item.title}</h3>

71c3c8688f11434a947b9dbd1dab901ca9264d66
김세진 <rlatpwls30@naver.com>
2024-08-06 10:37:27 +0900

    Refactor : 불필요한 authenticatedRawJsonRequest 함수 제거

--- client/itclips/src/api/apiUtils.ts
+++ client/itclips/src/api/apiUtils.ts
@@ -11,19 +11,6 @@ export const authenticatedRequest = (method: string, url: string, data?: any, pa
   });
 };
 
-// 인증된 요청과 raw json 전송을 위한 유틸리티 함수
-export const authenticatedRawJsonRequest = (method: string, url: string, data?: any, params?: any) => {
-  return api({
-    method,
-    url,
-    data,
-    params,
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
-};
-
 // 인증되지 않은 요청을 위한 유틸리티 함수
 export const request = (method: string, url: string, data?: any, params?: any) => {
   return api({

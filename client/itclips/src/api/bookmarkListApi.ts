aa5000575646b804a8779095eb8169111158a7a6
김세진 <rlatpwls30@naver.com>
2024-08-06 10:59:38 +0900

    Fix : axios 잘못된 method 호출 수정

--- client/itclips/src/api/bookmarkListApi.ts
+++ client/itclips/src/api/bookmarkListApi.ts
@@ -11,11 +11,11 @@ export const writeBookmarkListComment = (userId: number, listId: number, content
 };
 
 // 북마크리스트 댓글 수정
-export const editBookmarkListComment = (userId: number, commentId: number, content: string) => {
-  return authenticatedRequest("post", `/comment/update/${userId}/${commentId}`, { content }, { userId, commentId });
+export const editBookmarkListComment = (userId: number, commentId: number, contents: string) => {
+  return authenticatedRequest("put", `/comment/update/${userId}/${commentId}`, { contents }, { userId, commentId });
 };
 
 // 북마크리스트 댓글 삭제
 export const deleteBookmarkListComment = (userId: number, commentId: number) => {
-  return authenticatedRequest("post", `/comment/delete/${userId}/${commentId}`, undefined, { userId, commentId });
+  return authenticatedRequest("delete", `/comment/delete/${userId}/${commentId}`, undefined, { userId, commentId });
 };

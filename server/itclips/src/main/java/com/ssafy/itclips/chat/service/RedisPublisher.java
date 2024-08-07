d6ae8efe03ff47fef86598fa2ff792ba275e176c
김민솔 <artkms4815@naver.com>
2024-08-07 14:45:31 +0900

    feat : 메세지 올때 안읽은 메세지 카운트

--- server/itclips/src/main/java/com/ssafy/itclips/chat/service/RedisPublisher.java
+++ server/itclips/src/main/java/com/ssafy/itclips/chat/service/RedisPublisher.java
@@ -19,9 +19,6 @@ public class RedisPublisher {
 
     public void publish(ChannelTopic topic, MessageDTO message) {
         //TODO : 메세지 보낼 채팅 topic 없으면 db에 방찾고 없으면 에러 있으면 topic redis 에 넣고 메세지 보내기
-        log.info("Publishing message to {}", topic.getTopic());
-        log.info("Publishing message to {}", topic);
-        log.info("Message content: {}", message.getMessage());
         redisTemplate.convertAndSend(topic.getTopic(), message);
     }
 }

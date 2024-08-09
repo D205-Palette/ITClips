629be6327d8df1f3bf6cfdd6397d605aec2e678d
김세진 <rlatpwls30@naver.com>
2024-08-09 10:49:54 +0900

    Feat : 알림을 위한 sse연결 로직 추가

--- client/itclips/src/App.tsx
+++ client/itclips/src/App.tsx
@@ -3,17 +3,43 @@ import "./index.css";
 import { Outlet } from "react-router-dom";
 
 import NavBar from "./components/nav/NavBar";
+
+// stores
 import { useWebSocketStore } from "./stores/webSocketStore";
+import notificationStore from "./stores/notificationStore";
+import { authStore } from "./stores/authStore";
+
+// apis
+import { connectNotificationStream } from "./api/notificationApi";
 
 const App = () => {
 
   const connect = useWebSocketStore(state => state.connect);
   const disconnect = useWebSocketStore(state => state.disconnect);
+  const { addNotification } = notificationStore();
+  const userId = authStore(state => state.userId);
 
+  // webSocket 연결
   useEffect(() => {
     connect();
     return () => disconnect();
   }, [connect, disconnect]);
+
+  // sse 연결
+  useEffect(() => {
+    if (userId) {
+      const eventSource = connectNotificationStream(userId);
+
+      eventSource.onmessage = (event) => {
+        const newNotification = JSON.parse(event.data);
+        addNotification(newNotification);
+      };
+
+      return () => {
+        eventSource.close();
+      };
+    }
+  }, [userId]);
 
   return (
     <div className="App relative">

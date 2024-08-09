f9f5b72e419ba9fbae78b82efaeaf19534397b35
김세진 <rlatpwls30@naver.com>
2024-08-09 16:09:42 +0900

    Refactor : 코드 정리 및 주석 추가

--- client/itclips/src/App.tsx
+++ client/itclips/src/App.tsx
@@ -20,28 +20,29 @@ import { connectNotificationStream } from "./api/notificationApi";
 const App = () => {
   const connect = webSocketStore(state => state.connect);
   const disconnect = webSocketStore(state => state.disconnect);
-  const { addNotification } = notificationStore();
+  const { addNotification, fetchNotifications } = notificationStore();
   const fetchRooms = chatStore(state => state.fetchRooms);
   const updateTotalUnreadCount = chatStore(state => state.updateTotalUnreadCount);
-  const userId = authStore(state => state.userId);
+  const { userId, isLoggedIn } = authStore();
   const location = useLocation();
   // 특정 경로에 따라 클래스 적용
   const isIntroPage = location.pathname === '/intro'; // '/intro'를 인트로 페이지 경로로 변경
 
-  // webSocket 연결하면서 채팅방 목록 조회
+  // webSocket 연결하면서 채팅방 목록 조회 및 알림 가져오기
   useEffect(() => {
     const initializeChat = async () => {
       if (userId) {
         await fetchRooms(userId);
         updateTotalUnreadCount();
         connect();
+        fetchNotifications(userId);  // 로그인 시 알림 가져오기
       }
     };
 
     initializeChat();
 
     return () => disconnect();
-  }, [connect, disconnect, fetchRooms, updateTotalUnreadCount, userId]);
+  }, [connect, disconnect, fetchRooms, updateTotalUnreadCount, userId, fetchNotifications]);
 
   // sse 연결
   useEffect(() => {
@@ -50,14 +51,14 @@ const App = () => {
 
       eventSource.onmessage = (event) => {
         const newNotification = JSON.parse(event.data);
-        addNotification(newNotification);
+        addNotification({...newNotification, read: false});  // 새 알림을 읽지 않은 상태로 추가
       };
 
       return () => {
         eventSource.close();
       };
     }
-  }, [userId]);
+  }, [userId, addNotification]);
 
   return (
     <div className="App flex flex-col justify-between min-h-screen">

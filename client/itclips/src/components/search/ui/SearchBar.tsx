bca94825c4eb0ef3f2bb529a7611e9f8ac673f07
김세진 <rlatpwls30@naver.com>
2024-08-10 18:07:46 +0900

    Feat : 엔터키로 검색할 수 있게 추가

--- client/itclips/src/components/search/ui/SearchBar.tsx
+++ client/itclips/src/components/search/ui/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from "react";
 
 // components
 import SearchCategoryDropdown from "./SearchCategoryDropdown";
@@ -8,17 +8,22 @@ interface ChildProps {
 }
 
 const SearchBar: React.FC<ChildProps> = ({ handleCategory }) => {
+  
   const [ searchCategory, setSearchCategory ] = useState("카테고리");
   const [ searchKeyword, setSearchKeyword ] = useState("");
-
-  // const categories = ["카테고리", "유저", "북마크리스트", "로드맵", "태그"];
 
   const selectCategory = (category: string) => {
     setSearchCategory(category);
   };
 
+  // 엔터키를 눌러도 검색되도록
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleCategory(searchCategory, searchKeyword);
+  };
+
   return (
-    <div className="flex items-center space-x-2">
+    <form onSubmit={handleSubmit} className="flex items-center space-x-2">
       {/* 검색 카테고리 드롭다운 */}
       <SearchCategoryDropdown selectCategory={selectCategory} />
       {/* 검색바 */}
@@ -30,8 +35,8 @@ const SearchBar: React.FC<ChildProps> = ({ handleCategory }) => {
         onChange={(e) => setSearchKeyword(e.target.value)}
       />
       {/* 검색 버튼 */}
-      <button className="btn btn-primary" onClick={() => handleCategory(searchCategory, searchKeyword)}>검색</button>
-    </div>
+      <button type="submit" className="btn btn-primary">검색</button>
+    </form>
   );
 };
 

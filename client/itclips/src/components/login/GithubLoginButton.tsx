8be786ec7e0959c9ecf005b825e6f61a0e647e92
김동준 <kdj4355@naver.com>
2024-08-11 08:59:14 +0900

    Fix : 로그인 아이콘 변경

--- client/itclips/src/components/login/GithubLoginButton.tsx
+++ client/itclips/src/components/login/GithubLoginButton.tsx
@@ -1,10 +1,11 @@
 import { FaGithub } from "react-icons/fa";
-import { API_BASE_URL } from '../../config';
+import { API_BASE_URL } from "../../config";
+import github_logo from "../../assets/images/github_logo.svg";
 
 const GithubLoginButton = () => {
   const GITHUB_AUTH_URL = `${API_BASE_URL}:8084/oauth2/authorize/github`;
-  
-  const loginWithGithub = () => {    
+
+  const loginWithGithub = () => {
     window.location.href = GITHUB_AUTH_URL;
   };
 
@@ -14,7 +15,12 @@ const GithubLoginButton = () => {
       type="button"
       className="btn btn-outline bg-base-100 w-3/4"
     >
-      <FaGithub className="w-8 h-8" />
+      <img
+        className="w-8 h-8 bg-gray-100 border border-white rounded-full"
+        src={github_logo}
+        alt=""
+      />
+<FaGithub size={32}/>
       <p>깃허브 로그인</p>
     </button>
   );

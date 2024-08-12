55851851fa4a9bd999770e1d024cff6df6727dae
김동준 <kdj4355@naver.com>
2024-08-12 17:42:49 +0900

    Fix : 로그인 유무에 따른 라우터 접근 구분 추가

--- client/itclips/src/App.tsx
+++ client/itclips/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 // styles
 import "./index.css";
@@ -18,6 +18,7 @@ import toastStore from "./stores/toastStore";
 import { connectNotificationStream } from "./api/notificationApi";
 
 const App = () => {
+  const navigate = useNavigate();
   const connect = webSocketStore((state) => state.connect);
   const disconnect = webSocketStore((state) => state.disconnect);
   const { addNotification, fetchNotifications } = notificationStore();
@@ -27,9 +28,14 @@ const App = () => {
   );
   const { userId, isLoggedIn } = authStore();
   const location = useLocation();
+  const { globalNotification, setGlobalNotification } = toastStore();
+
+  // 로그인하지 않아도 접근 가능한 경로들
+  const publicRoutes = ["/intro", "/", "/signup", "/socialsignup", "/login"];
+
   // 특정 경로에 따라 클래스 적용
-  const isIntroPage = location.pathname === "/intro"; // '/intro'를 인트로 페이지 경로로 변경
-const {globalNotification, setGlobalNotification} = toastStore()
+  const isIntroPage = location.pathname === "/intro";
+
   // webSocket 연결하면서 채팅방 목록 조회 및 알림 가져오기
   useEffect(() => {
     const initializeChat = async () => {
@@ -44,15 +50,20 @@ const {globalNotification, setGlobalNotification} = toastStore()
     initializeChat();
 
     return () => disconnect();
-  }, [
-    connect,
-    disconnect,
-    fetchRooms,
-    updateTotalUnreadCount,
-    userId,
-    fetchNotifications,
-  ]);
-  
+  }, [connect, disconnect, fetchRooms, updateTotalUnreadCount, userId, fetchNotifications]);
+
+  // 로그인 상태에 따른 리다이렉트 처리
+  useEffect(() => {
+    if (!userId && !publicRoutes.includes(location.pathname)) {
+      // 현재 위치가 publicRoutes에 포함되지 않을 때만 리다이렉트
+      navigate('/login', { replace: true });
+      setGlobalNotification({
+        message: "로그인 후 접근 가능한 페이지입니다.", // 알림 메시지 출력
+        type: "error",
+      });
+    }
+  }, [userId, location.pathname, navigate]); // 로그인 상태 변경 시 useEffect 실행
+
   // 토스트 알람 메뉴
   useEffect(() => {
     if (globalNotification) {
@@ -81,14 +92,13 @@ const {globalNotification, setGlobalNotification} = toastStore()
   }, [userId, addNotification]);
 
   return (
-    <div className="App flex flex-col justify-between min-h-screen ">
+    <div className="App flex flex-col justify-between min-h-screen">
       <header className="App-header">
         <NavBar />
       </header>
 
       <main className="mt-16 w-full">
         <Outlet />
-
       </main>
 
       <footer
@@ -98,20 +108,19 @@ const {globalNotification, setGlobalNotification} = toastStore()
       >
         <Footer />
       </footer>
-      
-        {/* 토스트 알림창 */}
-        {globalNotification && (
-          <div
-            className={`fixed bottom-12 left-1/2 transform -translate-x-1/2 p-4 rounded-md ${
-              globalNotification.type === "success"
-                ? "bg-green-500"
-                : "bg-red-500"
-            } text-white shadow-lg z-50 transition-opacity duration-300`}
-          >
-            {globalNotification.message}
-          </div>
-        )}
-        
+
+      {/* 토스트 알림창 */}
+      {globalNotification && (
+        <div
+          className={`fixed bottom-12 left-1/2 transform -translate-x-1/2 p-4 rounded-md ${
+            globalNotification.type === "success"
+              ? "bg-green-500"
+              : "bg-red-500"
+          } text-white shadow-lg z-50 transition-opacity duration-300`}
+        >
+          {globalNotification.message}
+        </div>
+      )}
     </div>
   );
 };

ba5cdc015437f0f5fabf807308fcc46af65cab45
김동준 <kdj4355@naver.com>
2024-08-13 15:56:58 +0900

    Fix : 피드쪽 케밥드랍다운 URL, 즐겨찾기, 스크랩 수정

--- client/itclips/src/components/feed/KebabDropdown(Feed).tsx
+++ client/itclips/src/components/feed/KebabDropdown(Feed).tsx
@@ -7,6 +7,7 @@ import ScrapConfirmationModal from "../aside/modals/ScrapComfirmModal";
 import axios from "axios";
 import { API_BASE_URL } from "../../config";
 import { authStore } from "../../stores/authStore";
+import toastStore from "../../stores/toastStore";
 
 // 무슨 탭에서 눌렀는지 받는 인자
 // 리스트, 즐겨찾기, 로드맵, 북마크 4가지로 받을예정. 그룹 리스트랑 그냥 리스트는 차이 없음
@@ -23,74 +24,140 @@ const KebabDropdown: FC<Props> = ({ whatMenu, id }) => {
   const [isFavoriteModalOpen, setIsFavoriteModalOpen] =
     useState<boolean>(false);
   const [isScrapModalOpen, setIsScrapModalOpen] = useState<boolean>(false);
+  const { setGlobalNotification } = toastStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // 즐겨찾기 추가
+  // 리스트 즐겨찾기
   function addFavorite(): void {
-    axios.post(`${API_BASE_URL}/api/list/scrap/${userId}/${id}`,
-      {},
-      {
+    axios({
+      method: "post",
+      url: `${API_BASE_URL}/api/list/scrap/${userId}/${id}`,
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    });
+    })
+      .then(() => {
+        setIsMenuOpen(false);
+        setGlobalNotification({
+          message: "즐겨찾기 추가 완료",
+          type: "success",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          setIsMenuOpen(false);
+          axios.delete(`${API_BASE_URL}/api/list/scrap/${userId}/${id}`,{
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setGlobalNotification({
+            message: "즐겨찾기 삭제 완료",
+            type: "error",
+          });
+        } else {
+        }
+      });
   }
   // 로드맵 스크랩
   function addScrap(): void {
-    axios.post(`${API_BASE_URL}/api/roadmap/scrap/${id}/${userId}`,
-      {},
-      {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    
+    axios
+      .post(`${API_BASE_URL}/api/roadmap/scrap/${id}/${userId}`, {}, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {        
+        setIsMenuOpen(false);
+        setGlobalNotification({
+          message: "로드맵 스크랩 완료",
+          type: "success",
+        });
+      });
   }
-  
+
+  function copyUrl(whatMenu: string, id: number): any {
+    // 기본적으로 로컬호스트 주소를 가져옴
+
+    // whatMenu 값에 따라 URL을 생성
+    let url = "";
+    if (whatMenu === "리스트" || whatMenu === "즐겨찾기") {
+      url = `${API_BASE_URL}/bookmarklist/${id}`;
+    } else if (whatMenu === "로드맵") {
+      url = `${API_BASE_URL}/roadmap/${id}`;
+    } else {
+      console.error("Invalid menu type");
+      return;
+    }
+
+    // URL을 클립보드에 복사
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        // 복사가 성공적으로 완료되면 알림 설정
+        setGlobalNotification({
+          message: "URL 복사 완료",
+          type: "success",
+        });
+      })
+      .catch((err) => {
+        setGlobalNotification({
+          message: "URL 복사 실패",
+          type: "error",
+        });
+        console.error("Failed to copy URL: ", err);
+      });
+
+    // 메뉴 닫기
+    setIsMenuOpen(false);
+  }
+
   return (
     <>
       <div className="dropdown dropdown-bottom dropdown-end ">
-        <div tabIndex={0} role="button" className="btn m-1 btn-ghost ">
+        <div tabIndex={0} role="button" className="btn m-1 btn-ghost " onClick={() => setIsMenuOpen(true)}>
           <VscKebabVertical />
         </div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow z-30"
-        >
-          {/* 이구간은 내꺼 남꺼일때& 즐겨찾기일떄 유무 */}
-          <li onClick={() => setIsUrlCopyModalOpen(true)}>
-            <a>URL 복사하기</a>
-          </li>
-          <li
-            onClick={() => {
-              setIsFavoriteModalOpen(true);
-              addFavorite();
-            }}
-            className={
-              whatMenu === "로드맵" || whatMenu === "북마크" ? "hidden" : ""
-            }
+        {isMenuOpen && (
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow z-30"
           >
-            {/* 내 즐겨찾기에 있는지 유무 따져서 즐겨찾기 삭제로 출력해주기 */}
-            <a>즐겨찾기</a>
-          </li>
-          <li 
-          onClick={()=>{setIsScrapModalOpen(true); addScrap()}}
-          className={whatMenu === "로드맵" ? "" : "hidden"}>
-            <a>스크랩</a>
-          </li>
-          <li
-          onClick={()=>{setIsReportModalOpen(true);}}
-          className={whatMenu === "로드맵" ? "hidden" : ""}>
-            <a>신고하기</a>
-          </li>
-        </ul>
+            <li onClick={() => copyUrl(whatMenu, id)}>
+              <a>URL 복사하기</a>
+            </li>
+            <li
+              onClick={() => {                
+                addFavorite();
+              }}
+              className={
+                whatMenu === "로드맵" || whatMenu === "북마크" ? "hidden" : ""
+              }
+            >
+              {/* 내 즐겨찾기에 있는지 유무 따져서 즐겨찾기 삭제로 출력해주기 */}
+              <a>즐겨찾기</a>
+            </li>
+            <li
+              onClick={() => {
+                setIsScrapModalOpen(true);
+                addScrap();
+              }}
+              className={whatMenu === "로드맵" ? "" : "hidden"}
+            >
+              <a>스크랩</a>
+            </li>
+            <li
+              onClick={() => {
+                setIsReportModalOpen(true);
+              }}
+              className={whatMenu === "로드맵" ? "hidden" : ""}
+            >
+              <a>신고하기</a>
+            </li>
+          </ul>
+        )}
       </div>
 
-      {isUrlCopyModalOpen && (
-        <UrlCopyModal
-          isOpen={isUrlCopyModalOpen}
-          onClose={() => setIsUrlCopyModalOpen(false)}
-        />
-      )}
       {isReportModalOpen && (
         <ReportModal
           isOpen={isReportModalOpen}
@@ -98,19 +165,7 @@ const KebabDropdown: FC<Props> = ({ whatMenu, id }) => {
           whatContent={whatMenu}
           id={id}
         />
-      )}
-      {isFavoriteModalOpen && (
-        <FavoriteConfirmationModal
-          isOpen={isFavoriteModalOpen}
-          onClose={() => setIsFavoriteModalOpen(false)}
-        />
-      )}
-      {isScrapModalOpen && (
-        <ScrapConfirmationModal
-          isOpen={isScrapModalOpen}
-          onClose={() => setIsScrapModalOpen(false)}
-        />
-      )}
+      )}      
     </>
   );
 };

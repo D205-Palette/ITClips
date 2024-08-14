f9814dbd901ad2fb2ccce08ed47220ce50280e72
김동준 <kdj4355@naver.com>
2024-08-14 12:54:50 +0900

    Design : 푸터 디자인 변경

--- client/itclips/src/App.tsx
+++ client/itclips/src/App.tsx
@@ -31,7 +31,14 @@ const App = () => {
   const { globalNotification, setGlobalNotification } = toastStore();
 
   // 로그인하지 않아도 접근 가능한 경로들
-  const publicRoutes = ["/intro", "/", "/signup", "/socialsignup", "/login", "/oauth2/callback"];
+  const publicRoutes = [
+    "/intro",
+    "/",
+    "/signup",
+    "/socialsignup",
+    "/login",
+    "/oauth2/callback",
+  ];
 
   // 특정 경로에 따라 클래스 적용
   const isIntroPage = location.pathname === "/intro";
@@ -50,13 +57,20 @@ const App = () => {
     initializeChat();
 
     return () => disconnect();
-  }, [connect, disconnect, fetchRooms, updateTotalUnreadCount, userId, fetchNotifications]);
+  }, [
+    connect,
+    disconnect,
+    fetchRooms,
+    updateTotalUnreadCount,
+    userId,
+    fetchNotifications,
+  ]);
 
   // 로그인 상태에 따른 리다이렉트 처리
   useEffect(() => {
     if (!userId && !publicRoutes.includes(location.pathname)) {
       // 현재 위치가 publicRoutes에 포함되지 않을 때만 리다이렉트
-      navigate('/login', { replace: true });
+      navigate("/login", { replace: true });
       setGlobalNotification({
         message: "로그인 후 접근 가능한 페이지입니다.", // 알림 메시지 출력
         type: "error",
@@ -101,11 +115,7 @@ const App = () => {
         <Outlet />
       </main>
 
-      <footer
-        className={`${isIntroPage ? "" : "mt-32"} ${
-          isLoggedIn ? (window.innerWidth >= 768 ? "" : "mb-20") : ""
-        }`}
-      >
+      <footer>
         <Footer />
       </footer>
 
@@ -113,9 +123,7 @@ const App = () => {
       {globalNotification && (
         <div
           className={`fixed bottom-12 left-1/2 transform -translate-x-1/2 p-4 rounded-md ${
-            globalNotification.type === "success"
-              ? "bg-sky-400"
-              : "bg-red-500"
+            globalNotification.type === "success" ? "bg-sky-400" : "bg-red-500"
           } text-white shadow-lg z-50 transition-opacity duration-300`}
         >
           {globalNotification.message}
--- client/itclips/src/components/footer/Footer.tsx
+++ client/itclips/src/components/footer/Footer.tsx
@@ -11,38 +11,30 @@ const Footer = () => {
   };
 
   return (
-    <div className="bg-black w-full text-white py-3 px-6 md:px-20 text-left">
-      <div className="flex justify-between">
-        {/* 프로젝트명 및 카피라이트 */}
-        <div className="flex flex-col justify-between">
-          <h1 className="font-extrabold text-xl md:text-2xl">
-            <span className="text-sky-500">IT</span> Clips
-          </h1>
-          <p className="text-xs md:text-base">ⓒ ITClips Corp. All rights reserved.</p>
-        </div>
-
-        {/* 서비스 링크 및 팀 설명 */}
+    <div className="bg-base-100 w-full py-3 px-6 md:px-20 text-left">
+      <div className="flex flex-col items-center justify-center gap-1">
+        {/* 프로젝트명*/}
+        <h1 className="font-extrabold text-sm md:text-sm alfa-slab-one-regular">
+          <span className="text-sky-500 ">IT</span>Clips
+        </h1>
+        {/* 카피라이트 */}
+        <p className="text-xs md:text-xs">
+          ⓒ ITClips Corp. All rights reserved.
+        </p>
+        {/* 서비스 링크*/}
         <div className="flex gap-x-8 md:gap-x-16 items-start">
-          {/* 서비스 링크 - md 이하에서 숨김 */}
           <div className="hidden md:flex gap-x-4">
-            <h3 className="font-bold text-lg md:text-xl">서비스</h3>
             <div className="flex gap-x-3 items-center">
               {!isLoggedIn ? (
                 <>
                   <button
-                    className="text-left text-xs md:text-base hover:underline"
-                    onClick={handleNavigation("/")}
-                  >
-                    홈
-                  </button>
-                  <button
-                    className="text-left text-xs md:text-base hover:underline"
+                    className="text-left text-xs hover:underline"
                     onClick={handleNavigation("/login")}
                   >
                     로그인
                   </button>
                   <button
-                    className="text-left text-xs md:text-base hover:underline"
+                    className="text-left text-xs hover:underline"
                     onClick={handleNavigation("/signup")}
                   >
                     회원가입
@@ -51,50 +43,25 @@ const Footer = () => {
               ) : (
                 <>
                   <button
-                    className="text-left text-xs md:text-base hover:underline"
+                    className="text-left text-xs hover:underline"
                     onClick={handleNavigation(`/user/${userId}`)}
                   >
                     MY
                   </button>
                   <button
-                    className="text-left text-xs md:text-base hover:underline"
+                    className="text-left text-xs hover:underline"
                     onClick={handleNavigation("/feed/bookmarklists")}
                   >
                     피드
                   </button>
                   <button
-                    className="text-left text-xs md:text-base hover:underline"
+                    className="text-left text-xs hover:underline"
                     onClick={handleNavigation("/search")}
                   >
                     검색
                   </button>
                 </>
               )}
-            </div>
-          </div>
-
-          {/* 팀 설명 - lg 이하에서 숨김 */}
-          <div className="hidden lg:flex gap-x-5">
-            <h3 className="font-bold mb-2 text-xl">팀 팔레트</h3>
-            <div className="flex flex-col">
-              <div className="flex gap-x-3">
-                <div className="flex flex-col">
-                  <p className="font-bold">Backend</p>
-                  <p className="font-bold">Frontend</p>
-                </div>
-                <div className="flex flex-col">
-                  <p>정진규 팀장</p>
-                  <p>김동준 팀원</p>
-                </div>
-                <div className="flex flex-col">
-                  <p>김민솔 팀원</p>
-                  <p>김세진 팀원</p>
-                </div>
-                <div className="flex flex-col">
-                  <p>김수정 팀원</p>
-                  <p>최형우 팀원</p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
